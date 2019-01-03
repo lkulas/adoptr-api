5c870e02a31e65dba28e912a8ed2ab28fffaeff6
Laura Kulas <laura.kulas@gmail.com>
2019-01-03 16:09:45 -0700

    updated cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(morgan('common'));
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', CLIENT_ORIGIN);
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
