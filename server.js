e62c620993d57d5c60b5ea1d519008be61b7a654
Laura Kulas <laura.kulas@gmail.com>
2019-01-03 16:01:06 -0700

    updated CORS

--- server.js
+++ server.js
@@ -16,11 +16,15 @@ const app = express();
 
 app.use(morgan('common'));
 
-app.use(
-  cors({
-      origin: CLIENT_ORIGIN
-  })
-);
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
